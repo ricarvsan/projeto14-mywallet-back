48cbd59da9c73d9c9901cc44dcf17e9bff8a63c7
ricarvsan <ricarvsan@gmail.com>
2023-07-11 00:52:43 -0300

    back completo

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,4 +1,6 @@
 import { db } from '../database/database.connection.js';
+import dayjs from 'dayjs';
+
 
 export async function transaction(req, res) {
     const {token} = res.locals.session;
@@ -6,8 +8,21 @@ export async function transaction(req, res) {
 
     try {
         const user = await db.collection('session').findOne({token});
-        await db.collection('transactions').insertOne({userId: user.userId, type, value, description})
+        await db.collection('transactions').insertOne({userId: user.userId, type, value, description, date: dayjs().format('DD/MM')})
         res.sendStatus(200);
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function transactions(req, res) {
+    const {token} = res.locals.session;
+
+    try {
+        const user = await db.collection('session').findOne({token});
+        const transactions = (await db.collection('transactions').find({userId: user.userId}).toArray()).reverse();
+        res.status(200).send(transactions);
 
     } catch (err) {
         res.status(500).send(err.message)
--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { validateAuth } from "../middlewares/validateAuth.js";
-import { transaction } from "../controllers/transaction.controller.js";
+import { transaction, transactions } from "../controllers/transaction.controller.js";
 import { itemSchema } from "../schemas/transaction.schemas.js";
 
 
 const transactionsRouter = Router();
 
 transactionsRouter.post('/nova-transacao', validateAuth, validateSchema(itemSchema), transaction);
+transactionsRouter.get('/transacoes', validateAuth, transactions);
 
 export default transactionsRouter;
